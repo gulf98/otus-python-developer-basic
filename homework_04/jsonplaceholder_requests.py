93ce7b0b767486e2378229dcb30a98594d4f9a8c
Камский Евгений <kamskiie@gmail.com>
2023-11-24 06:44:17 +0300

    add homework_04

--- homework_04/jsonplaceholder_requests.py
+++ homework_04/jsonplaceholder_requests.py
@@ -2,5 +2,23 @@
 создайте асинхронные функции для выполнения запросов к ресурсам (используйте aiohttp)
 """
 
-USERS_DATA_URL = ""
-POSTS_DATA_URL = ""
+from aiohttp import ClientSession
+
+URL = "https://jsonplaceholder.typicode.com/"
+USERS_DATA_URL = f"{URL}/users"
+POSTS_DATA_URL = f"{URL}/posts"
+
+
+async def fetch_api_data(url: str) -> dict:
+    async with ClientSession() as session:
+        async with session.get(url) as response:
+            data = await response.json()
+            return data
+
+
+async def fetch_users_data():
+    return await fetch_api_data(USERS_DATA_URL)
+
+
+async def fetch_posts_data():
+    return await fetch_api_data(POSTS_DATA_URL)
--- homework_04/models.py
+++ homework_04/models.py
@@ -10,7 +10,71 @@
 
 import os
 
-PG_CONN_URI = os.environ.get("SQLALCHEMY_PG_CONN_URI") or "postgresql+asyncpg://postgres:password@localhost/postgres"
+from sqlalchemy import (
+    Column,
+    String,
+    Integer,
+    ForeignKey,
+    Text,
+)
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy.orm import sessionmaker
 
-Base = None
-Session = None
+PG_CONN_URI = os.environ.get("SQLALCHEMY_PG_CONN_URI") or "postgresql+asyncpg://postgres:password@localhost/postgres"
+DB_ECHO = False
+
+async_engine = create_async_engine(
+    url=PG_CONN_URI,
+    echo=DB_ECHO,
+)
+
+Session = sessionmaker(
+    bind=async_engine,
+    expire_on_commit=False,
+    class_=AsyncSession,
+)
+
+session: AsyncSession = Session()
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100), nullable=False, unique=False)
+    username = Column(String(100), nullable=False, unique=True)
+    email = Column(String(100), nullable=False, unique=True)
+    posts = relationship(
+        "Post",
+        back_populates="user",
+        uselist=True,
+    )
+
+
+class Post(Base):
+    __tablename__ = "posts"
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String(100), nullable=False, unique=False)
+    body = Column(
+        Text,
+        nullable=False,
+        unique=False,
+        default="",
+        server_default="",
+    )
+    user_id = Column(
+        Integer,
+        ForeignKey("users.id"),
+        unique=False,
+        nullable=False,
+    )
+    user = relationship(
+        "User",
+        back_populates="posts",
+        uselist=False,
+    )
